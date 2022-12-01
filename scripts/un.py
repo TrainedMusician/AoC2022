e11d13012bef402f51b2897eca1e5eac7d7ab77e
Job <job.mooij15@gmail.com>
2022-12-01 12:36:15 +0100

    First day finished

---
+++ scripts/un.py
@@ -0,0 +1,32 @@
+def read_file(file_path):
+    return open(file_path).read().split('\n')
+
+
+def find_max(inventory, maximum=0):
+    current_value = 0
+    for i in inventory:
+        if i:
+            current_value += int(i)
+        else:
+            maximum = max(current_value, maximum)
+            current_value = 0
+    return maximum
+
+
+def find_top(inventory, length=3):
+    top_hoarders = []
+    current_value = 0
+    for i in inventory:
+        if i:
+            current_value += int(i)
+        else:
+            top_hoarders.append(current_value)
+            if len(top_hoarders) > length:
+                top_hoarders.remove(min(top_hoarders))
+            current_value = 0
+    return top_hoarders
+
+
+if __name__ == '__main__':
+    print(find_max(read_file('../inputs/un.txt')))
+    print(sum(find_top(read_file('../inputs/un.txt'))))
